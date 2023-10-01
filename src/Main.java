51ea32544c2e3e31f1d8453a1c4323138783d7e2
Andrey.Skripay <lordktan@gmail.com>
2023-10-01 12:00:37 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+
+        int savings = 15_000; // Переменная сумма вклада
+        int total = 0; // Переменная сумма накопления
+        int month = 0;
+        while (total  < 2_459_000){
+            total = total + total/100;
+            total = total + savings;
+            month = total / savings;
+
+            System.out.println("Месяц " + month + " Итого " + total);
+        }
+
+
+
+
     }
 }
